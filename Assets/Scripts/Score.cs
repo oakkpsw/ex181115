29d09c77134e07668ffcf4ce1214850151132619
Patsawut Tananchai <oakpatsawut@gmail.com>
2015-11-21 18:15:02 +0700

    ะะะะ

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Score : MonoBehaviour {
+
+	int score = 0;
+	public Text txt;
+	public GameObject player;
+	
+	void Start () {
+		player = GameObject.FindWithTag("Player");
+		txt = gameObject.GetComponent<Text>(); 
+		txt.text = "Score :  " + score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		score = player.GetComponent<PlayerScore>().score;
+		txt.text = "Score :  " + score;
+
+		}
+
+
+}
---
+++ Assets/Scripts/ScoreCoin.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreCoin : MonoBehaviour {
+
+	public float speed;
+
+
+
+
+	void Start () {
+		speed = 360.0f;
+	}
+
+	void Update () {
+		transform.Rotate (0.0f, speed * Time.deltaTime, 0.0f);
+	
+	}
+	void OnTriggerEnter(Collider obj){
+
+		if(obj.gameObject.CompareTag("Player")){
+
+				obj.GetComponent<PlayerScore>().AddScore(1);
+
+			Destroy(gameObject);
+		}
+
+	}
+
+}
